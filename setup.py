3897c74e0f08e3b2c16ee0cd2fadad71a3d34ca5
Asheesh Laroia <asheesh@asheesh.org>
2012-09-07 23:58:41 -0700

    Everyone needs a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='autoresponse',
+    version='0.1dev',
+    packages=['autoresponse'],
+    license='Apache License 2.0',
+    long_description=open('README').read(),
+)
+
